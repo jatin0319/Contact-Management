24ef3006efaee13289cdc3a1551655de8080be1d
jatin0319 <jatinrwt19@gmail.com>
2023-05-24 02:14:49 +0530

    "Added Error Handler"

--- src/main/java/com/ContactManagement/Service/error/ErrorHandler.java
+++ src/main/java/com/ContactManagement/Service/error/ErrorHandler.java
@@ -2,6 +2,7 @@ package com.ContactManagement.Service.error;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -10,8 +11,8 @@ import java.time.Instant;
 @ControllerAdvice
 public class ErrorHandler {
 
-    @ExceptionHandler(value = RuntimeException.class)
-    public ResponseEntity<Object> exception(RuntimeException exception) {
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity<Object> exception(Exception exception) {
         ErrorDto error = ErrorDto.builder()
                 .message(exception.getMessage())
                 .error(HttpStatus.BAD_REQUEST.getReasonPhrase())
@@ -20,4 +21,5 @@ public class ErrorHandler {
                 .build();
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
+
 }
