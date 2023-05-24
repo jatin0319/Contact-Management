be84ed69d5645138552989b4d14256f9ab9fb36e
jatin0319 <jatinrwt19@gmail.com>
2023-05-24 12:36:55 +0530

    "project structure update"

--- src/main/java/com/service/contactmanagement/config/SwaggerConfig.java
+++ src/main/java/com/service/contactmanagement/config/SwaggerConfig.java
@@ -41,7 +41,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.ContactManagement.Service.controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.service.contactmanagement.controller"))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(getInfo())
