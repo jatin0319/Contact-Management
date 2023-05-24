bf4631ef72951fe904cb663bd2bd0f9b5f5467f8
jatin0319 <jatinrwt19@gmail.com>
2023-05-24 13:37:53 +0530

    "application properties update"

--- src/main/java/com/service/contactmanagement/service/token/CustomUserDetailsServiceImpl.java
+++ src/main/java/com/service/contactmanagement/service/token/CustomUserDetailsServiceImpl.java
@@ -22,10 +22,8 @@ public class CustomUserDetailsServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-
-        User user = userRepository.findByUsername(username);
-        if (user == null)
-            throw new UsernameNotFoundException("User not found");
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
         return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), new ArrayList<>());
     }
 }
